6314077487eed562a0176e51a2a2326165d374d2
Manuel17ms <manuel.manea@studenti.unitn.it>
2025-12-06 12:49:20 +0100

    Create Homeview.vue

---
+++ frontend/src/Views/Homeview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <SimulationForm />
+  </div>
+</template>
+
+<script>
+import SimulationForm from '../components/SimulationForm.vue';
+import { useSimulationStore } from '../store/simulationStore';
+
+export default {
+  name: 'HomeView',
+  components: { SimulationForm },
+  setup() {
+    const store = useSimulationStore();
+    store.loadLocations();
+
+    return {};
+  }
+};
+</script>
+
