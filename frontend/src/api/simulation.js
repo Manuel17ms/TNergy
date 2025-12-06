b97529a0dd3a53aa6bd0af2bd6207af20a812efd
Manuel17ms <manuel.manea@studenti.unitn.it>
2025-12-06 12:53:10 +0100

    Create simulation.js

---
+++ frontend/src/api/simulation.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://172.30.133.154:4000/api', // <- IP reale della macchina
+  headers: { 'Content-Type': 'application/json' }
+});
+
+export function postSimulation(data) {
+  return API.post('/simulation', data).then(res => res.data);
+}
+
+export function getLocations() {
+  return API.get('/localita').then(res => res.data);
+}
+
+
+export function getCompare(locationId) {
+  return API.get(`/simulation/compare/${locationId}`).then(res => res.data);
+}
